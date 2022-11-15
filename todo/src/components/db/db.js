d32874bb9c9325473a3a419c5921dae330522f96
baran-aa77 <rezvan.a78@gmail.com>
2022-11-15 23:36:33 +0330

    data added

---
+++ todo/src/components/db/db.js
@@ -0,0 +1,5 @@
+const Todos=[
+    {id:1 , title:'Title' , description:'say what...', isDone:false},
+    {id:2 , title:'Title1' , description:'say what......', isDone:true}
+  ]
+  export default Todos;
